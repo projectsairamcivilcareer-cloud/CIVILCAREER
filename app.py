7c3ff58b9ee8595c67bed09a914ec8a0eecb3e4d
projectsairamcivilcareer-cloud <projectsairamcivilcareer@gmail.com>
2026-09-23 22:00:03 +0530

    Use current GATE 2027 data by default

--- app.py
+++ app.py
@@ -675,7 +675,7 @@ def gate():
 
         return redirect(url_for("login"))
 
-    syllabus_year = request.args.get("year", "2026")
+    syllabus_year = request.args.get("year", "2027")
     if syllabus_year not in GATE_SYLLABI:
         syllabus_year = "2026"
 
